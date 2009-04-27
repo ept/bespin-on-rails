f499b53c78d4f08ee53736464ed44f72a6e13e7b
René Sprotte <rene.sprotte@provideal.net>
2009-04-27 09:29:29 +0200

    initial commit

---
+++ init.rb
@@ -0,0 +1,4 @@
+require 'bespin_rails'
+
+# Install (symlink) public files on plugin init
+install_public_files('bespin_rails')
---
+++ lib/bespin_rails.rb
@@ -0,0 +1,28 @@
+module BespinRails
+  class << self
+    def enable
+      require 'bespin_rails/view_helpers'
+      ::ActionView::Base.send :include, BespinRails::ViewHelpers
+    end
+  end
+end
+ 
+if defined?(Rails) and defined?(ActionController)
+  BespinRails.enable
+end
+
+#
+# Installes the plugins public files in the host application
+# by creating a proper symlink. The host app can access the
+# resources in /public/_plugins/<plugin_name>/...
+#
+def install_public_files(plugin_name)
+  rails_public_plugins_path = "#{RAILS_ROOT}/public/_plugins"
+  FileUtils.mkdir_p(rails_public_plugins_path)
+
+  rails_public_plugin_path  = "#{rails_public_plugins_path}/#{plugin_name}"
+  plugin_public_path        = "#{RAILS_ROOT}/vendor/plugins/#{plugin_name}/public"
+  if (File.exist?(plugin_public_path))
+    system "ln -s -f -h #{plugin_public_path} #{rails_public_plugin_path}"
+  end
+end
