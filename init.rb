b39516907fc0b0e4303bbb3fbffdbc0e929cc618
René Sprotte <renspr@maya.local>
2009-05-02 21:23:13 +0200

    Fixed a bug with resource linking

--- init.rb
+++ init.rb
@@ -1,4 +1,1 @@
 require 'bespin_on_rails'
-
-# Install (symlink) public files on plugin init
-install_public_files('bespin-on-rails')
--- lib/bespin_on_rails.rb
+++ lib/bespin_on_rails.rb
@@ -4,25 +4,30 @@ module BespinOnRails
       require 'bespin_on_rails/view_helpers'
       ::ActionView::Base.send :include, BespinOnRails::Helpers::FormTagHelper
     end
+    
+    def install_plugin_assets(plugin_name, target_path)
+      # Precheck conditions
+      raise "Plugin name required" unless plugin_name
+      raise "Target path required" unless target_path
+      raise "Taget does't exists"  unless File.exists?(target_path)
+    
+      # Create the plugins base path inside the hosts public folder
+      plugins_public_base_path = "#{RAILS_ROOT}/public/_plugins"
+      FileUtils.mkdir_p(plugins_public_base_path)
+    
+      # The source path
+      source_path = "#{plugins_public_base_path}/#{plugin_name}"
+    
+      # Create the symlink if the source_path is not present
+      # This allows for manual overrides
+      unless File.exists?(source_path)
+        system "ln -s #{target_path} #{source_path}"
+      end
+    end
   end
 end
  
-if defined?(Rails) and defined?(ActionController)
+if defined?(Rails) and defined?(ActionController) and defined?(ActionView)
+  BespinOnRails.install_plugin_assets('bespin-on-rails', File.expand_path(File.join(File.dirname(__FILE__), '..', 'public')))
   BespinOnRails.enable
-end
-
-#
-# Installes the plugins public files in the host application
-# by creating a proper symlink. The host app can access the
-# resources in /public/_plugins/<plugin_name>/...
-#
-def install_public_files(plugin_name)
-  rails_public_plugins_path = "#{RAILS_ROOT}/public/_plugins"
-  FileUtils.mkdir_p(rails_public_plugins_path)
-
-  rails_public_plugin_path  = "#{rails_public_plugins_path}/#{plugin_name}"
-  plugin_public_path        = "#{RAILS_ROOT}/vendor/plugins/#{plugin_name}/public"
-  if (File.exist?(plugin_public_path))
-    system "ln -s -f -h #{plugin_public_path} #{rails_public_plugin_path}"
-  end
 end
